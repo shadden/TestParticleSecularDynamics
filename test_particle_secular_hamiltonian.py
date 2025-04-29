2f633cb07f89cb8c90d817945d73672e80292cc4
Sam Hadden <shadden1107@gmail.com>
2025-04-29 16:06:23 -0400

    implemented linear theory solution

--- test_particle_secular_hamiltonian.py
+++ test_particle_secular_hamiltonian.py
@@ -1,10 +1,50 @@
 import numpy as np
 import celmech as cm
 from celmech.disturbing_function import df_coefficient_C, evaluate_df_coefficient_dict
+from celmech.poisson_series import PSTerm, PoissonSeries
+
+def list_multinomial_exponents(pwr,ndim):
+    """
+    List exponents appearing in the expansion of the multinomial 
+    
+    .. math:: 
+    (x_1 + x_2 + ... + x_\mathrm{ndim})^pwr
+
+    Parameters
+    ----------
+    pwr : int
+        Exponent of multinomial
+    ndim : int
+        Number of variables in multinomial
+
+    Returns
+    -------
+    list
+        A list of lists of length `ndim` representing the exponents of each
+        varaible :math:`x_i`
+    """
+    if ndim==1:
+        return [[pwr],]
+    else:
+        x =[]
+        for pow1 in range(0,pwr+1):
+            subpows = list_multinomial_exponents(pwr-pow1,ndim-1)
+            x+=[[pow1]+y for y in subpows]
+        return x
+    
+from math import factorial
+def multinomial_coefficient(p, ks):
+    """Calculate multinomial coefficient for given p and ks"""
+    num = factorial(p)
+    denom = 1
+    for k in ks:
+        denom *= factorial(k)
+    return num // denom
 
 def calc_g0_and_s0(semi_major_axis,synthetic_secular_theory,GM=1.0):
     """
-    Evaluate the lowest-order prediction for the secular free apsidal and nodal precession frequencies for a test particle.
+    Evaluate the lowest-order prediction for the secular free apsidal and nodal precession frequencies for a 
+    test particle.
 
     Parameters
     ----------
@@ -43,17 +83,120 @@ def calc_g0_and_s0(semi_major_axis,synthetic_secular_theory,GM=1.0):
     g0 *= 2*n
     s0 *= 0.5*n
     return g0,s0
+_p  = lambda k,nu: max(0,k) + nu
+_p1 = lambda k,nu: max(0,-k) + nu 
+_RT2 = np.sqrt(2)
 
+def test_particle_secular_terms(a,k,nu,synthetic_scular_system):
+    C_coeff = df_coefficient_C(k,nu)
+    for i in range(synthetic_scular_system.N_planets):
+        a_i = synthetic_scular_system.semi_major_axes[i]
+        if a<a_i:
+            alpha = a/a_i
+            C_i = evaluate_df_coefficient_dict(C_coeff,alpha)
+            pow_2_factor = _RT2**(2*nu[2]-2*nu[0] + np.abs(k[2])-np.abs(k[0]))
+            X_i_pow = _p(k[3],nu[3])
+            Xbar_i_pow = _p1(k[3],nu[3])
+            Y_i_pow = _p(k[5],nu[1])
+            Ybar_i_pow = _p1(k[5],nu[1])
+            
+            X_i_series_terms=[]
+            for pows in list_multinomial_exponents(X_i_pow,synthetic_scular_system.M_i[i]):
+                coeff = multinomial_coefficient(X_i_pow,pows)
+
+    
 class SyntheticSecularTheory():
     def __init__(self,masses,semi_major_axes,omega_vector,x_dicts,y_dicts):
         self.masses = masses
         self.semi_major_axes = semi_major_axes
+        self.N_planets = len(self.masses)
         self.N_freq = len(omega_vector)
         self.x_dicts = x_dicts
         self.y_dicts = y_dicts
+        self.M_i = np.array([len(x_d) for x_d in x_dicts],dtype=int)
+        self.N_i = np.array([len(y_d) for y_d in y_dicts],dtype=int)
+        self.omega_vector = omega_vector
+        #self.m_il = np.array([m for m in self.x_dicts.keys()],dtype=int)
+        #self.n_il = np.array([m for m in self.x_dicts.keys()],dtype=int)
+from collections import defaultdict
 
+def mathcal_X_dictionary(semi_major_axis,synthetic_secular_theory,GM=1):
+    n = np.sqrt(GM / semi_major_axis**3)
+    z4 = [0 for _ in range(4)]
+    C_e_mixed = df_coefficient_C(*[0,0,1,-1,0,0],*z4)
+    Xcal_dict = defaultdict(float)
+    for i in range(synthetic_secular_theory.N_planets):
+        a_i = synthetic_secular_theory.semi_major_axes[i]
+        m_i = synthetic_secular_theory.masses[i]
+        if a_i>semi_major_axis:
+            C  = (semi_major_axis/a_i) * evaluate_df_coefficient_dict(C_e_mixed,semi_major_axis/a_i)
+        else:
+            C  = evaluate_df_coefficient_dict(C_e_mixed,a_i/semi_major_axis)
+        x_dict = synthetic_secular_theory.x_dicts[i]
+        for mvec,amplitude in x_dict.items():
+            Xcal_dict[mvec] = m_i*n*C*amplitude
+    return Xcal_dict
 
+def mathcal_Y_dictionary(semi_major_axis,synthetic_secular_theory,GM=1):
+    n = np.sqrt(GM / semi_major_axis**3)
+    z4 = [0 for _ in range(4)]
+    C_I_mixed = df_coefficient_C(*[0,0,0,0,1,-1],*z4)
+    Ycal_dict = defaultdict(complex)
+    for i in range(synthetic_secular_theory.N_planets):
+        a_i = synthetic_secular_theory.semi_major_axes[i]
+        m_i = synthetic_secular_theory.masses[i]
+        if a_i>semi_major_axis:
+            C  = 0.5 * (semi_major_axis/a_i) * evaluate_df_coefficient_dict(C_I_mixed,semi_major_axis/a_i)
+        else:
+            C  = 0.5 * evaluate_df_coefficient_dict(C_I_mixed,a_i/semi_major_axis)
+        y_dict = synthetic_secular_theory.y_dicts[i]
+        for mvec,amplitude in y_dict.items():
+            Ycal_dict[mvec] = m_i*n*C*amplitude
+    return Ycal_dict
 
 class TestParticleSecularHamiltonian():
-    def __init__(self):
-        pass
+    def __init__(self,semi_major_axis,synthetic_secular_theory,GM=1.0):
+        """
+        Class representing the Hamiltonian of a test particle subject to secular forcing from a system of planets.
+
+        Parameters
+        ----------
+        semi_major_axis : float
+            The test particle's semi-major axis
+        synthetic_secular_theory : SyntheticSecularTheory
+            A representation of the planetary system's secular dynamics.
+        GM : float, optional
+            Newton's constant times the stellar mass, by default 1.0
+        """
+        self.semi_major_axis = semi_major_axis
+        self.synthetic_secular_theory = synthetic_secular_theory
+        self.g0,self.s0 = calc_g0_and_s0(semi_major_axis,synthetic_secular_theory,GM=GM)
+        mathcal_X_dict = mathcal_X_dictionary(semi_major_axis,synthetic_secular_theory)
+        mathcal_Y_dict = mathcal_Y_dictionary(semi_major_axis,synthetic_secular_theory)
+
+        # Get forced eccentricity
+        self.F_e = dict()
+        for m,amp in mathcal_X_dict.items():
+            omega_m = np.dot(m,self.omega_vector)
+            denom = self.g0 - omega_m
+            self.F_e[m] = -1*amp / denom
+
+        # Get forced inclination
+        self.F_inc = dict()
+        for m,amp in mathcal_Y_dict.items():
+            omega_m = np.dot(m,self.omega_vector)
+            denom = self.s0 - omega_m
+            self.F_inc[m] = -1*amp / denom
+
+    @property
+    def omega_vector(self):
+        return self.synthetic_secular_theory.omega_vector
+
+    def linear_theory_solution(self,x0,y0,times):
+        xForced = np.sum([amp * np.exp(1j * np.dot(m,self.omega_vector) * times) for m, amp in self.F_e.items()],axis=0)            
+        yForced = np.sum([amp * np.exp(1j * np.dot(m,self.omega_vector) * times) for m, amp in self.F_inc.items()],axis=0)
+        x0_free = x0 - xForced[0]
+        y0_free = y0 - yForced[0]
+        x_soln = x0_free * np.exp(1j * self.g0 * times) + xForced
+        y_soln = y0_free * np.exp(1j * self.s0 * times) + yForced
+        return x_soln,y_soln
